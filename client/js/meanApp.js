22f36930011ed68b062ddf003b7dc31076acd06f
Saurabh Shankariya <sshankariya@gmail.com>
2016-02-02 17:45:33 +0530

    Create meanApp.js
    Enter point at client side using angularjs

---
+++ client/js/meanApp.js
@@ -0,0 +1,7 @@
+/*
+*	Angular module defined for application.
+*	App has dependency on 'ngResource' for implementing RESTfull call
+*/
+var meanApp = angular.module('meanAppDemo', ['ngResource']).run(['$rootScope', function($rootScope) {
+	$rootScope.title = "MEAN Sample App";
+}]);
