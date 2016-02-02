d8638991c1e2020b4f1cf57aada84c57833f0055
Saurabh Shankariya <sshankariya@gmail.com>
2016-02-02 17:53:16 +0530

    Create landingPageController.js

---
+++ client/js/controllers/landingPageController.js
@@ -0,0 +1,41 @@
+
+// Angular based controller
+meanApp.controller('landingPageCtrl', ['$scope', '$resource', '$http', '$location', function($scope, $resource, $http, $location){
+	
+	//REST call urls
+	var Developers = $resource('/api/developers');
+	var DeleteDevelopers = $resource('/api/delete/:_id', {_id:'@id'})
+	
+	//Retrieve all developers on page load
+	Developers.query(function(result){
+		$scope.developers = result;
+	});
+	
+	//Reinitialize developers array
+	$scope.developers = [];
+
+	//Add new developer. This will make REST call to add new developer and in return update developers array to reflect on screen. 2 way binding
+	$scope.add = function(){
+		var develop = new Developers();
+		develop.name = $scope.dName;
+		develop.city = $scope.dCity;
+		develop.$save(function(result){
+			$scope.developers.push(result);
+			$scope.dName = "";
+			$scope.dCity = "";
+		});
+	};
+	
+	//Delete existing developer based on ID. This will make REST call to delete developer and in return update developers array to reflect on screen. 2 way binding
+	 $scope.deleteDeveloper = function(id) {
+        $http.delete('/api/delete/' + this.developer._id)
+            .success(function(data) {
+				Developers.query(function(result){
+					$scope.developers = result;
+				});
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+}]);
